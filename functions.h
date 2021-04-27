9fe8f3edb2a2a51cce351837308a481f6e480ee8
PLedwon <paul.ledwon@protonmail.com>
2021-04-27 14:57:38 +0200

    save

--- functions.h
+++ functions.h
@@ -3,66 +3,71 @@
 #include <cmath>
 #include <random>
 #include <vector>
+using std::vector;
 
-void printVector_(std::vector<double> a) {
+void printVector_(vector<double> a) {
     int i;
     for (i = 0; i < a.size(); i++) std::cout << a[i] << ' ';
     printf("\n");
 }
 
-double avg(std::vector<double> a) {
+double avg(vector<double> a) {
     double average=std::accumulate(a.begin(),a.end(),0.0);
     return (double) average/a.size();
 }
 
-
-
-
-void computeMasses(std::vector<double> masses, double oscMass,double M, std::vector<double>  omega, double omegaMin,const double GAMMA){
+vector<double> computeMasses(double oscMass,double M, vector<double>  omega, double omegaMin,const double GAMMA){
+  vector<double> masses(omega.size()+1,0);
   masses.front()=M;
   for (int i = 1; i < masses.size() ; i++) {
-    masses[i]=oscMass*pow((omega[i]/omegaMin),(GAMMA-3))*exp(-omega[i]);
+    masses[i]=oscMass*pow((omega[i-1]/omegaMin),(GAMMA-3))*exp(-omega[i-1]);
   }
+  return masses;
+
 }
 
-void computeSpringConstants(std::vector<double> k ,std::vector<double> masses, std::vector<double> omega) {
+vector<double> computeSpringConstants(vector<double> masses, vector<double> omega) {
   //int n = nElems(k);
-  k[0]=0.0;
+  vector<double> k(masses.size(),0);
   for (int i = 1; i < k.size(); i++) {
-    k[i]=masses[i]*pow(omega[i],2);
+    k[i]=masses[i]*pow(omega[i-1],2);
   }
+  return k;
 }
 
-double H(double q[] , double p[], double k[], double invM[], const int N) { // compute total energy of the system
+double H(vector<double> q , vector<double> p, vector<double> k, vector<double> invM) { // compute total energy of the system
   double E = p[0] * p[0] * invM[0];
-  for (int i = 1; i < N+1 ; ++i) {
+  for (int i = 1; i < q.size() ; ++i) {
     E += p[i] * p[i] * invM[i] + k[i] * pow(q[i] - q[0], 2);
     }
   E *= 0.5;
   return  E;
 }
 
-double sum(std::vector<double> p) {
+double sum(vector<double> p) {
     double mom = std::accumulate(p.begin(),p.end(),0.0);
     return mom;
 }
 
-void setEigenfrequencies(std::vector<double> omega, double omegaMin, double omegaMax) {
+vector<double> setEigenfrequencies(double omegaMin, double omegaMax, int N) {
     double c;
+    vector<double> omega(N,0);
     c = (omegaMax - omegaMin)/(omega.size()-1);
     for(int i = 0; i < omega.size() - 1; ++i) // equidistant distribution of eigenfrequencies of the harmonic oscillators
         omega[i] = omegaMin + i*c;
     omega.back() = omegaMax;
-    printVector_(omega);
+    return omega;
 }
 
-void invertMasses(std::vector<double> invM, double M, std::vector<double> masses) {
+vector<double> invertMasses(double M, vector<double> masses) {
+    vector<double> invM(masses.size(),0);
     for (int i = 0; i < invM.size() ; ++i) {
        invM[i] = 1/masses[i];
     }
+    return invM;
 }
 
-void generateInitialConditions(std::vector<double> q0, std::vector<double> p0, double M, std::vector<double> masses, std::vector<double> k,const double BETA) {
+void generateInitialConditions(vector<double> q0, vector<double> p0, double M, vector<double> masses, vector<double> k,const double BETA) {
 
     std::random_device rd{};
     std::mt19937 gen{rd()};
@@ -81,18 +86,15 @@ void generateInitialConditions(std::vector<double> q0, std::vector<double> p0, d
     //initialize heatbath with vanishing center of mass velocity
     double avgMomentum = avg(p0);
     double p0sum = sum(p0);
-    printf("total momentum before: %E \n", p0sum);
-    printVector_(p0);
     for (int i = 0; i < p0.size(); ++i) {
         p0[i] -= avgMomentum;
     }
     p0sum = sum(p0);
-    printf("avg momentum before shift: %E \n", avgMomentum);
-    printf("total momentum after shift: %E \n", p0sum);
-    printVector_(p0);
+     printf("total momentum after shift: %E \n", p0sum);
+   // printVector_(p0);
 }
 
-void updateMomenta(std::vector<double> p1, std::vector<double> q0, std::vector<double> p0, std::vector<double> k,const double DT,const int N) {
+void updateMomenta(vector<double> p1, vector<double> q0, vector<double> p0, vector<double> k,const double DT,const int N) {
     double s;
     p1[0] = p0[0];
     for (int i = 1; i < N+1; ++i) {
@@ -102,18 +104,18 @@ void updateMomenta(std::vector<double> p1, std::vector<double> q0, std::vector<d
     }
 }// have to be of same length
 
-void updatePositions(std::vector<double> q1, std::vector<double> p1, std::vector<double> q0, std::vector<double> invM, const double DT, const int N) {
+void updatePositions(vector<double> q1, vector<double> p1, vector<double> q0, vector<double> invM, const double DT, const int N) {
     for (int i = 0; i < N+1 ; ++i) {
        q1[i] = q0[i] + p1[i]*invM[i]*DT;
     }
 }
 
-void makeTimestep(std::vector<double> q1, std::vector<double> p1, std::vector<double> q0,std::vector<double> p0, std::vector<double> k, std::vector<double> invM,const double DT,const int N) {
+void makeTimestep(vector<double> q1, vector<double> p1, vector<double> q0,vector<double> p0, vector<double> k, vector<double> invM,const double DT,const int N) {
     updateMomenta(p1,q0,p0,k,DT,N); //update momenta first for a symplectic Euler algorithm
     updatePositions(q1,p1,q0,invM,DT,N);
 }
 
-void solveEOM(std::vector<double> q1, std::vector<double> p1, std::vector<double> q0, std::vector<double> p0, std::vector<double> invM, std::vector<double> k, const double TSPAN[], const double DT, const int N) {
+void solveEOM(vector<double> q1, vector<double> p1, vector<double> q0, vector<double> p0, vector<double> invM, vector<double> k, const double TSPAN[], const double DT, const int N) {
     int nTimesteps = ceil((TSPAN[1]-TSPAN[0])/DT);
     double initialEnergy;
     for (int i = 0; i < nTimesteps ; ++i) {
--- heatbath.cpp
+++ heatbath.cpp
@@ -3,38 +3,38 @@
 #include <cmath>
 #include <vector>
 #include "functions.h"
+using std::vector;
 
 int main() {
 
-const int N = 500; //# of harmonic oscillators in our heatbath
-const int NTOTAL = N + 1;
+const int N = 1000; //# of harmonic oscillators in our heatbath
+const int NTOTAL = N + 1; // adding the distinguished particle
 const double GAMMA = 1.2; // expected superdiffusion exponent
 const double BETA = 1.0; //kB*T
 const double TSPAN[2] = {0, pow(10,1)};
-const double DT = pow(10,-2);
+const double DT = pow(10,-4);
 double oscMass = 1.0; //mass of heaviest bath oscillator
 double M = 1.0; // mass of distinguished particle
 double omegaMin=pow(N,-0.7988), omegaMax=omegaMin*pow(N,1.0688); //highest and lowest eigenfrequency of the bath
-//double omega[N];
-std::vector<double> omega(N,0);
-std::vector<double> masses(NTOTAL,0);
-std::vector<double> k(NTOTAL,0);
-std::vector<double> invM(NTOTAL,0);
-std::vector<double> q0(NTOTAL,0);
-std::vector<double> p0(NTOTAL,0);
-std::vector<double> q1(NTOTAL,0);
-std::vector<double> p1(NTOTAL,0);
+//vector<double> invM(NTOTAL,0);
+vector<double> q0(NTOTAL,0);
+vector<double> p0(NTOTAL,0);
+vector<double> q1(NTOTAL,0);
+vector<double> p1(NTOTAL,0);
 
 
 // setting the bathparameters
-setEigenfrequencies(omega,omegaMin,omegaMax);
-computeMasses(masses,oscMass,M,omega,omegaMin,GAMMA);
-computeSpringConstants(k, masses, omega);
-invertMasses(invM, M, masses);
+const vector<double> omega = setEigenfrequencies(omegaMin,omegaMax,N);
+const vector<double> masses = computeMasses(oscMass,M,omega,omegaMin,GAMMA);
+const vector<double> k = computeSpringConstants(masses, omega);
+const vector<double> invM = invertMasses(M, masses);
 // set initial conditions and solve the eom
 generateInitialConditions(q0,p0,M,masses,k,BETA);
 
-//solveEOM(q1,p1,q0,p0,invM,k,TSPAN,DT,N);
+printf("Energy before solving heatbath: %E", H(q0,p0,k,invM));
+solveEOM(q1,p1,q0,p0,invM,k,TSPAN,DT,N);
+printf("Energy after solving heatbath: %E", H(q1,p1,k,invM));
+printf("momentum after solving heatbath: %E", std::accumulate(p1.begin(),p1.end(),0)) ;
 
 // solve EOM and save Q at times tsave
 //save Q to file
