bcea0471b5241fe0432c304ebe9aa9aa62fff22c
PLedwon <paul.ledwon@protonmail.com>
2021-05-21 07:30:56 +0200

    sync stuff

--- plots/plots.py
+++ plots/plots.py
@@ -28,7 +28,7 @@ if not glob.glob('./*.npz'):
 
 norm = np.power(len(resultList),-1.0)
 varQ = norm*squaredQ - np.power(norm,2.0)*aveQ
-plt.plot(varQ[0:100:np.size(q)-1])
+plt.plot(varQ[0:10:np.size(q)-1])
 plt.savefig("./img/varQ.pdf")
 
 
