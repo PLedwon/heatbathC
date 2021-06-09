e77cd55f92b254a795ce43fac2fdde29672e2e7d
PLedwon <paul.ledwon@protonmail.com>
2021-06-09 09:11:41 +0200

    sync stuff

--- plots/plots.py
+++ plots/plots.py
@@ -37,7 +37,7 @@ t = t * dts
 gamma=1.6
 
 def theoDiff(x,a,b):
-    return a*np.power(x,gamma)+b
+    return a*np.power(x,gamma)#+b
 
 #startIndex = int(math.floor,t1/dt*0.5)
 startIndex = int(np.floor(len(t)*0.01))
